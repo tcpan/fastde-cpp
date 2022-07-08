bf42beaad3d9caa9dadf63cd860f2f764bfb899b
Tony Pan <tpan7@gatech.edu>
2022-07-08 15:20:12 -0400

    WIP: support large h5

---
+++ R/read_h5.R
@@ -0,0 +1,97 @@
+
+library(rhdf5)
+
+
+
+#' Read 10X hdf5 file
+#'
+#' Read count matrix from 10X CellRanger hdf5 file.
+#' This can be used to read both scATAC-seq and scRNA-seq matrices.
+#' modified from Seurat's version to allow for very large sparse matrix
+#'
+#' @rdname Read10X_h5_big
+#' @param filename Path to h5 file
+#' @param use.names Label row names with feature names rather than ID numbers.
+#' @param unique.features Make feature names unique (default TRUE)
+#'
+#' @return Returns a sparse matrix with rows and columns labeled. If multiple
+#' genomes are present, returns a list of sparse matrices (one per genome).
+#'
+#' @name Read10X_h5_big
+#' @export
+#' @concept preprocessing
+#'
+Read10X_h5_big <- function(filename, use.names = TRUE, unique.features = TRUE) {
+  if (!requireNamespace('hdf5r', quietly = TRUE)) {
+    stop("Please install hdf5r to read HDF5 files")
+  }
+  if (!file.exists(filename)) {
+    stop("File not found")
+  }
+  infile <- hdf5r::H5File$new(filename = filename, mode = 'r')
+  genomes <- names(x = infile)
+  output <- list()
+  if (hdf5r::existsGroup(infile, 'matrix')) {
+    # cellranger version 3
+    if (use.names) {
+      feature_slot <- 'features/name'
+    } else {
+      feature_slot <- 'features/id'
+    }
+  } else {
+    if (use.names) {
+      feature_slot <- 'gene_names'
+    } else {
+      feature_slot <- 'genes'
+    }
+  }
+  for (genome in genomes) {
+    counts <- infile[[paste0(genome, '/data')]]
+    indices <- infile[[paste0(genome, '/indices')]]
+    indptr <- infile[[paste0(genome, '/indptr')]]
+    shp <- infile[[paste0(genome, '/shape')]]
+    features <- infile[[paste0(genome, '/', feature_slot)]][]
+    barcodes <- infile[[paste0(genome, '/barcodes')]]
+    # TCP: using spam for sparse matrix.
+    print(shp)
+    sparse.mat <- spam(0, shp[1], shp[2])
+    sparse.mat@entries <- as.numeric(x = counts[])
+    sparse.mat@colindices <- as.numeric(x = indices[] + 1)
+    sparse.mat@rowpointers <- as.numeric(x = indptr[])
+    # TCP: end spam...
+
+    if (unique.features) {
+      features <- make.unique(names = features)
+    }
+    # TCP: spam has no support for col and row name (dimanmes for non arrays.)
+    # rownames(x = sparse.mat) <- features
+    # colnames(x = sparse.mat) <- barcodes[]
+    # below is now needed.
+    # sparse.mat <- as(object = sparse.mat, Class = 'dgCMatrix')
+
+    # TCP:  this is not yet tested, but should be okay...
+    # Split v3 multimodal
+    if (infile$exists(name = paste0(genome, '/features'))) {
+      types <- infile[[paste0(genome, '/features/feature_type')]][]
+      types.unique <- unique(x = types)
+      if (length(x = types.unique) > 1) {
+        message("Genome ", genome, " has multiple modalities, returning a list of matrices for this genome")
+        sparse.mat <- sapply(
+          X = types.unique,
+          FUN = function(x) {
+            return(sparse.mat[which(x = types == x), ])
+          },
+          simplify = FALSE,
+          USE.NAMES = TRUE
+        )
+      }
+    }
+    output[[genome]] <- sparse.mat
+  }
+  infile$close_all()
+  if (length(x = output) == 1) {
+    return(output[[genome]])
+  } else{
+    return(output)
+  }
+}
